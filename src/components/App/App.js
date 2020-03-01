baacf9a3aeaf41f6a5bc579263c993c980756e8e
giulianaferraro <giulianaferrarocamus@gmail.com>
2020-03-01 02:24:10 +0000

    Add Switch to toggle app mode

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,36 @@
-import React from 'react';
-import { Layout } from 'antd';
+import React, { useState } from 'react';
+import { Layout, Switch } from 'antd';
 import Logo from '../Logo/Logo';
-import Homepage from '../Homepage/Homepage';
+import Homepage from '../Homepage';
+import FilterForm from '../FilterForm';
 import './App.css';
 
 const { Header, Content, Footer } = Layout;
 
 function App() {
+  const [traditionalMode, setTraditionalMode] = useState(true);
+  const handleChange = () => {
+    setTraditionalMode(!traditionalMode);
+  };
+
   return (
     <Layout className="layout app-container" theme="dark" style={{ minHeight: '100%' }}>
       <Header className="header" style={{ height: 'auto', lineHeight: 'inherit' }}>
-        <div style={{ margin: 'auto' }}><a href="https://movieroulette.herokuapp.com/" title="MovieRoulette"><Logo /></a></div>
+        <a href="https://movieroulette.herokuapp.com/" title="MovieRoulette"><Logo /></a>
+        <Switch
+          checkedChildren="Traditional"
+          unCheckedChildren="Custom"
+          defaultChecked
+          onChange={handleChange}
+        />
       </Header>
       <Content className="main-container" style={{ height: '100%' }}>
         <div style={{ background: '#fff', height: '100%', margin: 'auto' }}>
-          <Homepage />
+          {
+            traditionalMode
+              ? <Homepage />
+              : <FilterForm />
+          }
         </div>
       </Content>
       <Footer style={{ textAlign: 'center' }}>
